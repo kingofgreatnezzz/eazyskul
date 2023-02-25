6b3374578f108075d70e2122eba751d8874975b1
unknown <kingofgreatnezzz@gmail.com>
2023-02-25 11:36:38 -0800

    taa

--- my_tin/urls.py
+++ my_tin/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
      path('admin/', admin.site.urls),
      path('halls/', include('halls.urls', namespace="halls")),
-     path('', include('imt.urls')),    
+     path('imt/', include('imt.urls')),    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

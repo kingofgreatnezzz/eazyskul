740be6eca6914e5eb819f05d0ceee991b1dbf3da
unknown <kingofgreatnezzz@gmail.com>
2023-02-25 01:50:50 -0800

    ;;

--- my_tin/settings.py
+++ my_tin/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+   # "django.middleware.security.SecurityMiddleware",
+   # "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,4 +142,4 @@ LOGOUT_REDIRECT_URL = 'index'
 # Email configurations 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"

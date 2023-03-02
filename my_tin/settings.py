f818580a0f1e5046c55a8c4ebba3490f173bf5d5
unknown <kingofgreatnezzz@gmail.com>
2023-03-02 09:09:58 -0800

    >

--- my_tin/settings.py
+++ my_tin/settings.py
@@ -120,12 +120,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]  
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+"""STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'"""
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

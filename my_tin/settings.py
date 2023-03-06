25791e3c1f6afd7d221ebbbdc2c7b560bafd4df2
unknown <kingofgreatnezzz@gmail.com>
2023-03-06 10:47:21 -0800

    hot

--- my_tin/settings.py
+++ my_tin/settings.py
@@ -82,7 +82,6 @@ WSGI_APPLICATION = 'my_tin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
